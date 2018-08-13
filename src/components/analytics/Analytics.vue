67a50f16a978b22da347ed59269e47aad4bc99c5
jeub <jurgen.palsma@gmail.com>
2018-08-13 12:20:53 +0100

    all business data treated and charts integrated

--- src/components/analytics/Analytics.vue
+++ src/components/analytics/Analytics.vue
@@ -6,7 +6,7 @@
                     Analytics
                 </h1>
             </div>
-
+            <date-picker v-model="daterange" lang='fr' range :shortcuts="shortcuts"></date-picker>
             <div class="level">
                 <div class="level-item has-text-centered">
                     <div>
@@ -25,15 +25,42 @@
                     <p class="heading">Tickets closed</p>
                     <p class="title">{{n_tickets_closed}}</p>
                     </div>
-                </div>
-                
+                </div> 
             </div>
 
             <div> 
                 <pie-chart :messages="{empty: 'Pas de tickets fermés a ce jour'}" :data="chart_tickets" :library="{animation: { easing: 'easeOutQuad' }}"></pie-chart>
             </div>
 
-            
+            <div>
+                <area-chart :data="tickets_created_per_day" :library="{animation: { easing: 'easeOutQuad' }}"></area-chart>
+            </div>
+            <!-- TODO: Set router link to ticket -->
+            <div class="level">
+                    <div class="level-item has-text-centered">      
+                        <div>
+                            <p class="heading">Shortest ticket</p>
+                            <p class="title">{{shortest_ticket_time}}</p>
+                            {{shortest_ticket.title}}
+                        </div>
+                    </div>
+                <!-- TODO: Set router link to ticket -->
+                    <div class="level-item has-text-centered">      
+                        <div>
+                            <p class="heading">Average resolution time</p>
+                            <p class="title">{{avg_ticket_time}}</p>
+                        </div>
+                    </div>
+                <!-- TODO: Set router link to ticket -->
+                    <div class="level-item has-text-centered">      
+                        <div>
+                            <p class="heading">Longest Ticket</p>
+                            <p class="title">{{longest_ticket_time}}</p>
+                            {{longest_ticket.title}}
+                        </div>
+                    </div>
+            </div>
+
         </div>
     
     </section>
@@ -42,17 +69,42 @@
 <script>
 
 import AnalyticsService from '@/services/AnalyticsService'
+import DatePicker from 'vue2-datepicker'
 
 export default {
     name: 'Analytics',
 
     data () {
         return {
+            /* Stats data */
             n_tickets_open: 0,
             n_tickets_pending: 0,
             n_tickets_closed: 0,
+            chart_tickets: [],
+            tickets_created_per_day: [],
+            avg_ticket_time: 0,
+            shortest_ticket: {_id: null, title: null},
+            shortest_ticket_time: null,
+            longest_ticket: {_id: null, title: null},
+            longest_ticket_time: null,
+
+            /* DatePicker */
             daterange: [],
-            chart_tickets: []
+            from: '',
+            to: '',
+            shortcuts: [
+                {
+                    text: 'Today',
+                    onClick: () => {
+                        this.daterange = [ new Date(), new Date() ]
+                    }
+                }
+            ],
+            timePickerOptions: {
+                start: '00:00',
+                step: '00:30',
+                end: '23:30'
+            }
         }
     },
     mounted: function () {
@@ -62,13 +114,28 @@ export default {
         async load () {
             const resp = await AnalyticsService.getStats(this.$cookies.get('api_token'), this.daterange)
             if (resp.data.success) {
-                console.log(resp.data)
                 this.n_tickets_open = resp.data.ticket_numbers.tickets_open
                 this.n_tickets_pending = resp.data.ticket_numbers.tickets_pending
                 this.n_tickets_closed = resp.data.ticket_numbers.tickets_closed
                 this.chart_tickets = resp.data.chart_tickets
+                this.tickets_created_per_day = resp.data.tickets_created_per_day
+                this.avg_ticket_time = resp.data.ticket_times.avg_ticket_time
+                this.shortest_ticket = resp.data.ticket_times.shortest_ticket
+                this.shortest_ticket_time = resp.data.ticket_times.shortest_ticket_time
+                this.longest_ticket = resp.data.ticket_times.longest_ticket
+                this.longest_ticket_time = resp.data.ticket_times.longest_ticket_time
+            } else {
+                alert('Bad date range')
             }
         }
+    },
+    watch: {
+        daterange: function (val) {
+            this.load()
+        }
+    },
+    components: {
+        DatePicker
     }
 }
 </script>
--- src/services/AnalyticsService.js
+++ src/services/AnalyticsService.js
@@ -3,20 +3,29 @@ import UserService from '@/services/UserService'
 
 export default {
     async getStats (token, daterange) {
+        let to = ''
+        let from = ''
+        if (daterange[0] && daterange[1]) {
+            from = daterange[0].toISOString()
+            to = daterange[1].toISOString()
+        }
         let res = await Api().get('/api/reporting/stats', {
             headers: {
                 'x-access-token': token,
-                'from': '',
-                'to': ''
+                'from': from,
+                'to': to
             }
         })
-        console.log(res.data.caretaker_numbers.tickets)
-        res.data.chart_tickets = await addTicketCloserToTickets(token, res.data.caretaker_numbers.tickets)
+        if (res.data.success) {
+            res.data.chart_tickets = await addTicketCloserToTicketList(token, res.data.caretaker_numbers.tickets)
+            res.data.tickets_created_per_day = addDatetoTicketList(res.data.ticket_numbers.tickets_created_per_day)
+            res.data.ticket_times = formatTicketTime(res.data.ticket_numbers.avg_resolution_time, res.data.ticket_numbers.shortest_ticket, res.data.ticket_numbers.longest_ticket)
+        }
         return res
     }
 }
 
-async function addTicketCloserToTickets (token, tickets) {
+async function addTicketCloserToTicketList (token, tickets) {
     let tArray = []
     for (let i = 0; i < tickets.length; i++) {
         let res = await UserService.getUser(token, tickets[i]._id)
@@ -25,4 +34,30 @@ async function addTicketCloserToTickets (token, tickets) {
         }
     }
     return tArray
+}
+
+function addDatetoTicketList (tickets) {
+    let tArray = []
+    for (let i = 0; i < tickets.length; i++) {
+        tArray.push([tickets[i].date, tickets[i].amount])
+    }
+    return tArray
+}
+
+function formatTimeHours (avg) {
+    // TODO: format string so we dont get weird floats for time avg but get "jour, mois, etc"
+    if (avg.toFixed(1) !== 0) {
+        return avg.toFixed(1) + 'h'
+    } else {
+        return '<1h'
+    }
+}
+
+function formatTicketTime (avg, short, long) {
+    return { 'avg_ticket_time': formatTimeHours(avg),
+        'shortest_ticket': short,
+        'longest_ticket': long,
+        'shortest_ticket_time': formatTimeHours(short.resolution_time),
+        'longest_ticket_time': formatTimeHours(long.resolution_time)
+    }
 }
