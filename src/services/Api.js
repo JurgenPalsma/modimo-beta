dee478e6cb81c2f983cba991e3b9c8b536cc847a
jeub <jurgen.palsma@gmail.com>
2018-09-06 11:11:47 +0200

    fix url api

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
     return axios.create({
-        baseURL: 'http://localhost:8000' // `https://modimo-beta-api.herokuapp.com/`
+        baseURL: 'https://modimo-beta-api.herokuapp.com/'
     })
 }
