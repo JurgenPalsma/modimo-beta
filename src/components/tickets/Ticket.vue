02e84d1966036f6f8d6f2d3af432071958482f5c
EtiBan <etienne.bansard@epitech.eu>
2018-09-08 11:57:02 +0200

    little fixes

--- src/components/tickets/Ticket.vue
+++ src/components/tickets/Ticket.vue
@@ -8,19 +8,19 @@
                     <button class="delete is-pulled-right" aria-label="close" @click="$emit('close_modal')"></button>
                     <div class="media-content">
                         <div class="content">
-                        <p>
-                            <strong class="modimo-color modimo-size"><!--{{ticket.author_id}}-->Jean-Pierre_Gardien - {{ticket.title}}</strong>
-                            <br>
-                            {{ticket.content}}
-                            <br>
-                            <small class="small-text">
-                                <span v-if="ticket.updated_at.$date === ticket.created_at.$date">Crée le </span>
-                                <span v-else>Modifié le </span>
-                                {{ticket.updated_at.$date}}
-                                <!-- ICI IL FAUT AFFICHER SEULEMENT SI L'AUTEUR EST LA PERSONNE CONNECTEE ET LANCER UNE MODAL ptetre celle de creation de ticket POUR MODIFIER LE TICKET-->
-                                <!--<span v-if="ticket.author_id === session._id">--> · <a>Modifier ticket</a><!--</span>-->
-                            </small>
-                        </p>
+                            <p>
+                                <strong class="modimo-color modimo-size"><!--{{ticket.author_id}}-->Jean-Pierre_Gardien - {{ticket.title}}</strong>
+                                <br>
+                                {{ticket.content}}
+                                <br>
+                                <small class="small-text">
+                                    <span v-if="ticket.updated_at.$date === ticket.created_at.$date">Crée le </span>
+                                    <span v-else>Modifié le </span>
+                                    {{ticket.updated_at.$date}}
+                                    <!-- ICI IL FAUT AFFICHER SEULEMENT SI L'AUTEUR EST LA PERSONNE CONNECTEE ET LANCER UNE MODAL ptetre celle de creation de ticket POUR MODIFIER LE TICKET-->
+                                    <!--<span v-if="ticket.author_id === session._id">--> · <a>Modifier ticket</a><!--</span>-->
+                                </small>
+                            </p>
                         </div>
                         <article class="media">
                             <div class="media-content comment">
@@ -43,18 +43,18 @@
                     <!--</article>-->
                     <br>
                     <article class="media">
-                    <div class="media-content">
-                        <div class="field">
-                        <p class="control">
-                            <textarea class="textarea" rows="1" placeholder="Add a comment..."></textarea>
-                        </p>
+                        <div class="media-content">
+                            <div class="field">
+                                <p class="control">
+                                    <textarea class="textarea" rows="1" placeholder="Add a comment..."></textarea>
+                                </p>
+                            </div>
+                            <div class="field">
+                                <p class="control">
+                                    <button class="button">Commenter</button>
+                                </p>
+                            </div>
                         </div>
-                        <div class="field">
-                        <p class="control">
-                            <button class="button">Commenter</button>
-                        </p>
-                        </div>
-                    </div>
                     </article>
                 </div>
             </div>
@@ -64,60 +64,57 @@
 </template>
 
 <script>
-export default {
-    name: 'ticket',
-    data () {
-        return {
-            isActive: true,
-            ticket: {
-                '_id': '123133',
-                'title': 'Du feu dans la cheminée',
-                'content': 'mon ascenceur est cassé, j\'ai envie d\'une pomme.',
-                'author_id': '1238EeGZY3',
-                'residence_id': '1e238aEeGZY3',
-                'status': 'open',
-                'created_at': {
-                    '$date': '2018-08-13T10:09:26.236Z'
-                },
-                'updated_at': {
-                    '$date': '2018-08-13T10:12:47.414Z'
-                },
-                'comments': [
-                    {
-                        '_id': '123133',
-                        'author_id': 'NadineDu6eme',
-                        'content': 'c\'est un vrai problème',
-                        'created_at': {
-                            '$date': '2018-08-14T10:09:26.236Z'
-                        },
-                        'updated_at': {
-                            '$date': '2018-08-14T10:09:26.236Z'
-                        }
+    export default {
+        name: 'ticket',
+        data () {
+            return {
+                isActive: true,
+                ticket: {
+                    '_id': '123133',
+                    'title': 'Du feu dans la cheminée',
+                    'content': 'mon ascenceur est cassé, j\'ai envie d\'une pomme.',
+                    'author_id': '1238EeGZY3',
+                    'residence_id': '1e238aEeGZY3',
+                    'status': 'open',
+                    'created_at': {
+                        '$date': '2018-08-13T10:09:26.236Z'
                     },
-                    {
-                        '_id': '123134',
-                        'author_id': 'GastonRDC',
-                        'content': 'Je suis d\'accord',
-                        'created_at': {
-                            '$date': '2018-08-14T10:09:26.236Z'
+                    'updated_at': {
+                        '$date': '2018-08-13T10:12:47.414Z'
+                    },
+                    'comments': [
+                        {
+                            '_id': '123133',
+                            'author_id': 'NadineDu6eme',
+                            'content': 'c\'est un vrai problème',
+                            'created_at': {
+                                '$date': '2018-08-14T10:09:26.236Z'
+                            },
+                            'updated_at': {
+                                '$date': '2018-08-14T10:09:26.236Z'
+                            }
                         },
-                        'updated_at': {
-                            '$date': '2018-08-14T10:12:47.414Z'
+                        {
+                            '_id': '123134',
+                            'author_id': 'GastonRDC',
+                            'content': 'Je suis d\'accord',
+                            'created_at': {
+                                '$date': '2018-08-14T10:09:26.236Z'
+                            },
+                            'updated_at': {
+                                '$date': '2018-08-14T10:12:47.414Z'
+                            }
                         }
-                    }
-                ],
-                'votes': [
-                    '123133'
-                ]
+                    ],
+                    'votes': [
+                        '123133'
+                    ]
+                }
             }
-        }
-    },
-    methods: {}
-}
-
+        },
+        methods: {}
+    }
 </script>
 
-<style lang="scss">
-
-       
+<style lang="scss">       
 </style>
