102f1d1546d3afe3fe6050baf7494b3a378456af
EtiBan <etienne.bansard@epitech.eu>
2018-09-09 10:35:10 +0200

    gestion front modif ticket

--- src/components/tickets/Ticket.vue
+++ src/components/tickets/Ticket.vue
@@ -3,24 +3,36 @@
         <div class="modal is-active">
             <div class="modal-background" style="opacity:50%;" @click="$emit('close_modal')"></div>
             <div class="modal-content">
-                <!-- Any other Bulma elements you want -->
                 <div class="box">
                     <button class="delete is-pulled-right" aria-label="close" @click="$emit('close_modal')"></button>
                     <div class="media-content">
                         <div class="content">
-                            <p>
-                                <strong class="modimo-color modimo-size"><!--{{ticket.author_id}}-->Jean-Pierre_Gardien - {{ticket.title}}</strong>
+                            <strong class="modimo-color modimo-size"><!--{{ticket.author_id}}-->Jean-Pierre_Gardien - {{ticket.title}}</strong>
+                            <br>
+                            <span ref="display_ticket">{{ticket.content}}</span>
+                            <!--ICI LE CHAMP QUI APPARAIT POUR LA MODIF DU MESSAGE-->
+                            <div ref="space_modif_ticket" class="media-content" v-bind:style="{display: 'none'}">
                                 <br>
-                                {{ticket.content}}
-                                <br>
-                                <small class="small-text">
-                                    <span v-if="ticket.updated_at.$date === ticket.created_at.$date">Crée le </span>
-                                    <span v-else>Modifié le </span>
-                                    {{ticket.updated_at.$date}}
-                                    <!-- ICI IL FAUT AFFICHER SEULEMENT SI L'AUTEUR EST LA PERSONNE CONNECTEE ET LANCER UNE MODAL ptetre celle de creation de ticket POUR MODIFIER LE TICKET-->
-                                    <!--<span v-if="ticket.author_id === session._id">--> · <a>Modifier ticket</a><!--</span>-->
-                                </small>
-                            </p>
+                                <div class="field">
+                                    <p class="control">
+                                        <textarea ref="text_modif_ticket" v-bind:value="ticket.content" class="textarea" rows="1"></textarea>
+                                    </p>
+                                </div>
+                                <div class="field">
+                                    <p class="control is-pulled-right">
+                                        <button class="button" v-on:click="cancelModifTicket">Annuler</button>&nbsp;
+                                        <button class="button">Modifier</button>
+                                    </p>
+                                </div>
+                            </div>
+                            <br>
+                            <small class="small-text">
+                                <span v-if="ticket.updated_at.$date === ticket.created_at.$date">Crée le </span>
+                                <span v-else>Modifié le </span>
+                                {{ticket.updated_at.$date}}
+                                <!-- ICI IL FAUT AFFICHER SEULEMENT SI L'AUTEUR EST LA PERSONNE CONNECTEE -->
+                                <!--<span v-if="ticket.author_id === session._id">--> · <a v-on:click="modifTicket">Modifier ticket</a><!--</span>-->
+                            </small>
                         </div>
                         <article class="media">
                             <div class="media-content comment">
@@ -58,7 +70,6 @@
                     </article>
                 </div>
             </div>
-            <!--<button class="modal-close is-large" aria-label="close" @click="$emit('close_modal')"></button>-->
         </div>
     </section>
 </template>
@@ -68,11 +79,12 @@
         name: 'ticket',
         data () {
             return {
+                isNone: 'none;',
                 isActive: true,
                 ticket: {
                     '_id': '123133',
                     'title': 'Du feu dans la cheminée',
-                    'content': 'mon ascenceur est cassé, j\'ai envie d\'une pomme.',
+                    'content': 'mon ascenseur est cassé, j\'ai envie d\'une pomme.',
                     'author_id': '1238EeGZY3',
                     'residence_id': '1e238aEeGZY3',
                     'status': 'open',
@@ -86,7 +98,7 @@
                         {
                             '_id': '123133',
                             'author_id': 'NadineDu6eme',
-                            'content': 'c\'est un vrai problème',
+                            'content': 'c\'est un vrai problème !',
                             'created_at': {
                                 '$date': '2018-08-14T10:09:26.236Z'
                             },
@@ -97,7 +109,7 @@
                         {
                             '_id': '123134',
                             'author_id': 'GastonRDC',
-                            'content': 'Je suis d\'accord',
+                            'content': 'Je suis d\'accord.',
                             'created_at': {
                                 '$date': '2018-08-14T10:09:26.236Z'
                             },
@@ -112,7 +124,16 @@
                 }
             }
         },
-        methods: {}
+        methods: {
+            modifTicket: function (event) {
+                this.$refs.space_modif_ticket.style = 'display: block;'
+                this.$refs.display_ticket.style = 'display: none;'
+            },
+            cancelModifTicket: function (event) {
+                this.$refs.space_modif_ticket.style = 'display: none;'
+                this.$refs.display_ticket.style = 'display: block;'
+            }
+        }
     }
 </script>
 <style lang="scss">       
