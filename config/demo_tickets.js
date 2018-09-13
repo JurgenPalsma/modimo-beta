4718939c4063d2480e9ba34ea611a0caeacc7195
jeub <jurgen.palsma@gmail.com>
2018-09-13 22:47:19 +0200

    fix ticket status

--- config/demo_tickets.js
+++ config/demo_tickets.js
@@ -27,7 +27,7 @@ module.exports = [
     {
             'title': 'Ascenseur en panne',
             'content': 'L\'ascenseur du bâtiment A ne marche plus. Il est arrêté entre le deuxième et troisième étage. Merci de contacter OTIS afin qu\'ils interviennent',
-            'status': 'close',
+            'status': 'closed',
             'created_at': {
                 '$date': '2018-07-05T08:01:28.327Z'
             },
@@ -51,7 +51,7 @@ module.exports = [
     {
             'title': 'Fenêtre premier étage cassée',
             'content': 'Fenêtre dans la cage d\'escalier du 1er étage ne ferme plus. Pour éviter tout cambriolage, il faudrait que le gardien intervienne dans les plus brefs délais',
-            'status': 'close',
+            'status': 'closed',
             'created_at': {
                 '$date': '2018-06-25T06:34:28.337Z'
             },
@@ -75,7 +75,7 @@ module.exports = [
     {
         'title': 'Dégradation parking',
         'content': 'Le parking sous terrain à été victime de tagueur la nuit dernière, il faudrait effacer ces graffitis qui sont, pour certains violents',
-        'status': 'close',
+        'status': 'closed',
         'created_at': {
             '$date': '2018-07-15T09:42:42.427Z'
         },
