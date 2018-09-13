dddd07fee3dcf3125f1ecf82e6a7a12be27a6699
Goop <stephane.lejeune@epitech.eu>
2018-09-13 14:33:21 +0200

    close-background-modal

--- src/components/tickets/TicketCreation.vue
+++ src/components/tickets/TicketCreation.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <div class="modal is-active">
-            <div class="modal-background"></div>
+            <div class="modal-background" @click="$emit('close_modal')"></div>
                 <div class="modal-card">
                 <header class="modal-card-head">
                     <p class="modal-card-title">Nouveau Ticket</p>
