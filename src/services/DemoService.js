768fec0f3e3704948ca1d94a5e1bb4968f056c1b
jeub <jurgen.palsma@gmail.com>
2018-09-13 15:24:00 +0200

    fix

--- src/services/DemoService.js
+++ src/services/DemoService.js
@@ -3,11 +3,19 @@ import Api from '@/services/Api'
 export default {
 
     create_resident_demo (email) {
+        dataLayer.push({
+            'event': 'api call',
+            'action': 'signup resi'
+        });
         return Api().post('/api/demo/resident', {
             'email': email
         })
     },
     create_admin_demo (email) {
+        dataLayer.push({
+            'event': 'api call',
+            'action': 'signup admin'
+        });
         return Api().post('/api/demo/admin', {
             'email': email
         })
