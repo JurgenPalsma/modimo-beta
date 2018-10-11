ef8151f5905b1b8314c0f5066d524525699a50eb
Pablo Giraud-Carrier <pablo.giraud-carrier@epitech.eu>
2018-10-11 15:05:37 +0200

    responsive app ticket

--- src/components/tickets/Tickets.vue
+++ src/components/tickets/Tickets.vue
@@ -5,87 +5,73 @@
 
 <template>
 <!--Give_UserIdto Component-->
-    <section class="section is-fullheight-minus-navbar modimo-dark">
-        <div class="container">
-            <div class="title has-text-centered white-title">
-                Tickets
-                <a @click="showModalTicketCreation = true" class="super-button">+</a>
-            </div>
-            <div style="position:relative">
-                <div class="buttons has-addons is-centered">
-                    <span class="button" :class="index === 0 ? 'is-info' : ''" @click="index = 0">Ouverts</span>
-                    <span class="button" :class="index === 1 ? 'is-info' : ''" @click="index = 1">Tous</span>
-                    <span class="button" :class="index === 2 ? 'is-info' : ''" @click="index = 2">Fermés</span>
+    <section class="hero is-fullheight-minus-navbar modimo-dark">
+        <div class="hero-body">
+            <div class="container">
+                <div class="title has-text-centered white-title">
+                    Tickets
+                    <a @click="showModalTicketCreation = true" class="super-button">+</a>
                 </div>
-                <div style="position:absolute;top:0;right:0">
-                    <div class="dropdown" :class="dropdownVisible ? 'is-active' : ''" @click="dropdownVisible = !dropdownVisible">
-                        <div class="dropdown-trigger">
-                            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                            <span>{{this.sortBy[this.sortIndex]}}</span>
-                            <span class="icon is-small">
-                                <i class="fas fa-angle-down" aria-hidden="true"></i>
-                            </span>
-                            </button>
+                <div class="columns">
+                    <div class="column is-12-mobile is-4-tablet is-offset-4-tablet">
+                        <div class="buttons has-addons is-centered">
+                            <span class="button" style="width: 33.3%" :class="index === 0 ? 'is-info' : ''" @click="index = 0">Ouverts</span>
+                            <span class="button" style="width: 33.4%" :class="index === 1 ? 'is-info' : ''" @click="index = 1">Tous</span>
+                            <span class="button" style="width: 33.3%" :class="index === 2 ? 'is-info' : ''" @click="index = 2">Fermés</span>
                         </div>
-                        <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                            <div class="dropdown-content">
-                                <a v-for="(sort, index) in sortBy" :key="sort" @click="sortIndex = index" class="dropdown-item" :class="index === sortIndex ? 'is-active' : ''">
-                                    {{sort}}
-                                </a>
+                    </div>
+                    <div class="column is-12-mobile is-3-tablet is-offset-1-tablet">
+                        <div class="dropdown" :class="dropdownVisible ? 'is-active' : ''" @click="dropdownVisible = !dropdownVisible">
+                            <div class="dropdown-trigger">
+                                <button class="button is-fullwidth" aria-haspopup="true" aria-controls="dropdown-menu">
+                                <span>{{this.sortBy[this.sortIndex]}}</span>
+                                <span class="icon is-small">
+                                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                                </button>
+                            </div>
+                            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                                <div class="dropdown-content">
+                                    <a v-for="(sort, index) in sortBy" :key="sort" @click="sortIndex = index" class="dropdown-item" :class="index === sortIndex ? 'is-active' : ''">
+                                        {{sort}}
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <br>
-                <div class="tile">
-                    <div class="tile is-vertical">
-                    <div v-for="ticket in showTickets" :key="ticket._id"
-                        class="tile is-child background-tile" style="border-radius: 3px; padding: 10px 0">
-                        <a @click="idToModal(ticket)" style="color: #4a4a4a">
-                            <div class="columns is-centered">
-                                <div class="column is-mobile">
-                                    <div id="ticket-status" class="has-text-centered icon-status">
-                                        <i v-if="ticket.status === 'open'" class="fas fa-bell fa-3x"></i>
-                                        <i v-else-if="ticket.status === 'closed'" class="fas fa-lock fa-3x"></i>
-                                    </div>
-                                </div>
-                                <div class="column is-one-third is-mobile">
-                                    <div id="ticket-title" class="has-text-left title-section">
-                                        <p class="bold modimo-color modimo-size"> {{ ticket.title }} </p>
-                                        <p class="bold">Créé par : </p> {{ticket.author_id}}
-                                    </div>
-                                </div>
-                                <div class="column is-one-quarter is-mobile">
-                                    <div id="ticket-time" class="has-text-left">
-                                            <p class="bold">Créé le : </p>
-                                            <time :datetime="ticket.created_at">{{ dateFormater(ticket.created_at) }}</time> <br>
-                                            <p class="bold">Derniere modification : </p>
-                                            <time :datetime="ticket.updated_at">{{ dateFormater(ticket.updated_at) }}</time>
-                                    </div>
-                                </div>
-                                <div class="column is-one-third is-mobile">
-                                    <div id="ticket-advancement" class="has-text-left">
-                                        <p class="bold">Avancement :</p>
-                                        <div v-if="ticket.status === 'open'" class="animated-advancement">
-                                            <p class="circle-processUp">Ouvert</p>
-                                        </div>
-                                        <div v-else-if="ticket.status === 'closed'" class="animated-advancement">
-                                            <p class="circle-processDown">Fermé</p>
+                <br>
+                <div class="columns is-multiline">
+                    <div v-for="ticket in showTickets" :key="ticket._id" class="column modimo-tiny-padding is-12">
+                        <div class="modimo-tile">
+                            <a @click="idToModal(ticket)" style="color: #4a4a4a">
+                                <div class="columns is-vcentered is-mobile is-multiline" style="margin:0">
+                                    <div class="column is-2-mobile is-1-desktop">
+                                        <div id="ticket-status" class="has-text-centered icon-status">
+                                            <i v-if="ticket.status === 'open'" class="fas fa-bell fa-2x"></i>
+                                            <i v-else-if="ticket.status === 'closed'" class="fas fa-lock fa-2x"></i>
                                         </div>
                                     </div>
-                                    <div id="ticket-vote" class="has-text-left">
-                                        <p class="bold">vote :</p>
-                                        <i class="far fa-thumbs-up"></i> {{ ticket.votes.length}}
+                                    <div class="column is-8-mobile is-4-desktop">
+                                        <p class="bold modimo-color modimo-title-size is-text-overflow has-text-centered-mobile"> {{ ticket.title }} </p>
+                                    </div>
+                                    <div class="column is-8-mobile is-4-desktop">
+                                        <p class="bold modimo-content-size is-text-overflow">Créé le <time :datetime="ticket.created_at" class="no-bold">{{ dateFormater(ticket.created_at) }}</time></p>
+                                        <p class="bold modimo-content-size is-text-overflow">par <span class="no-bold">{{ticket.author_id}}</span></p>
+                                    </div>
+                                    <div class="column is-4-mobile is-3-desktop has-text-right">
+                                        <span v-if="ticket.status === 'open'" class="bold circle-processUp">Ouvert</span>
+                                        <span v-else-if="ticket.status === 'closed'" class="bold circle-processDown">Fermé</span>
+                                        <p class="bold"><i class="far fa-thumbs-up"/> {{ ticket.votes.length}}</p>
                                     </div>
                                 </div>
-                            </div>
-                        </a>
-                        <ticket :ticket="currentTicket" v-show="showModalTicket" @close_modal="showModalTicket = false"></ticket>
+                            </a>
+                            <ticket :ticket="currentTicket" v-show="showModalTicket" @close_modal="showModalTicket = false"></ticket>
+                        </div>
                     </div>
                 </div>
+                <ticketCreation v-show="showModalTicketCreation" @close_modal="closeModalTicketCreation"></ticketCreation>
             </div>
-            <ticketCreation v-show="showModalTicketCreation" @close_modal="closeModalTicketCreation"></ticketCreation>
         </div>
     </section>
 </template>
@@ -131,7 +117,7 @@ export default {
             this.showTickets = this.sortTickets(newIndex);
         },
         sortIndex: function(newIndex) {
-            this.showTickets = this.sortTickets(newIndex);
+            this.showTickets = this.sortTickets(this.index);
         }
     },
     methods: {
@@ -175,7 +161,7 @@ export default {
         },
 
         dateFormater(unFormatedDate) {
-            var date = moment(String(unFormatedDate)).format('MM/DD/YYYY hh:mm')
+            var date = moment(String(unFormatedDate)).format('MM/DD/YY à hh:mm')
             return (date)
         }
     },
