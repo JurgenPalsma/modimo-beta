7a724e97c6ae274f71b00e5e7667382286ebcc4b
Raphael <raphael.moisset.grimm@gmail.com>
2018-11-08 15:50:38 +0100

    adding billboard route

--- src/router/index.js
+++ src/router/index.js
@@ -50,6 +50,11 @@ export default new Router({
             path: '/analytics',
             name: 'Analytics',
             component: Analytics
+        },
+        {
+            path: '/billboard',
+            name: 'Billboard',
+            component: Billboard
         }
     ]
 })
