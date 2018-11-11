4f8ba212b1ed4ea52486e871d91d8efc734d4d3f
jurgen <jurgen.palsma@gmail.com>
2018-11-11 19:12:40 +0100

    add call pour creation de demo depuis la landing

--- routes/routes-demo.js
+++ routes/routes-demo.js
@@ -18,7 +18,7 @@ let create_lead = function(role, email) {
 
 module.exports = function(app, apiRoutes) {
 
-    let create_demo_res = function(user_is_caretaker=false) {
+    let create_demo_res = function(residence_name="Une résidence démo", user_is_caretaker=false) {
         // TODO: Parametrize all this with config files
         let default_app_ids = [
             "5a774ac0734d1d3bd58cefc7",
@@ -30,13 +30,13 @@ module.exports = function(app, apiRoutes) {
 
         let caretaker = new User({
             name: c_name,
-            password: "super secret",
+            password: "super secret password",
             application_list: default_app_ids,
             roles: ["CARETAKER", "ADMIN"]
         });
 
         let residence = new Residence({
-            name: "Une résidence démo",
+            name: residence_name,
             caretaker_id: caretaker._id
         });
 
@@ -100,6 +100,56 @@ module.exports = function(app, apiRoutes) {
             return({success: true});
         }
     }
+
+    
+
+    apiRoutes.post('/demo', function(req, res) {
+        
+        // Check email
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(String(req.body.email).toLowerCase())) { return res.json({success: false, message: 'bad email'});}
+
+        // Check params
+        if (!req.body.email ||
+            !req.body.residenceName  ||
+            !req.body.firstname ||
+            !req.body.lastname ||
+            !req.body.password ||
+            !req.body.roles  )
+            return res.json({success:false, message:'Bad params, need: (req.body.email && req.body.residenceName && req.body.firstname && req.body.lastname && req.body.password && req.body.roles)'})
+
+        // Create demo residence
+        resi = create_demo_res(name=req.body.residenceName)
+        if (resi.success == false) return res.json(resi)
+        
+        // create user
+        let user = new User({
+            name: (req.body.firstname + ' ' + req.body.lastname),
+            first_name : req.body.firstname,
+            last_name : req.body.lastname,
+            password: req.body.password,
+            email: req.body.email,
+            residence: resi.residence._id, // TODO check if res exists
+            roles: req.body.roles,
+            application_list: resi.residence.default_app_ids,
+        });
+
+        // save the user
+        user.save(function(err) {
+            if (err) throw err;
+            else {
+                let ticket_r = fill_demo_tickets(user, resi.residence._id, resi.caretaker._id)
+                if (!ticket_r.success) return res.json(ticket_r)
+                
+                let lead_r = create_lead('RESIDENT', req.body.email)
+                if (!lead_r.success) return res.json(lead_r)
+
+                return res.json({success: true, message: "User registered", user: user})
+            }
+        });
+    });
+
+
 
     apiRoutes.post('/demo/resident', function(req, res) {
         
