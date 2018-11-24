8231b681aaee31980838f2357d1116ee8a074eb6
Pablo Giraud-Carrier <pablo.giraud-carrier@epitech.eu>
2018-11-24 21:00:04 +0100

    front messagerie

--- src/components/messaging/Messaging.vue
+++ src/components/messaging/Messaging.vue
@@ -11,34 +11,28 @@
                     </div>
                 </div>
                 <div class="columns is-multiline is-mobile">
-                    <div class="column">
-                        <h2 class="title white-title is-4">
-                            Conversations
-                        </h2>
-                        <div class="row">
-                            <div v-for="conv in conversations" :key="conv._id" @click="switchToConversation(conv)" class="column card is-full" style="border-radius: 3px; position: relative; cursor: pointer"> 
+                    <div class="column is-6-tablet is-12-mobile">
+                        <div class="modimo-conversations">
+                            <div v-for="conv in conversations" :key="conv._id" @click="switchToConversation(conv)" class="column card is-full" style="border-radius: 3px; position: relative; cursor: pointer; margin-bottom: 10px"> 
                                 <h1 style="font-weight: bold;">{{conv.name}}</h1>
                                 <p class="conv-date">{{dateFormater(new Date(conv.messages[conv.messages.length - 1].timestamp).toString())}}</p>
                                 <p>{{conv.messages[conv.messages.length - 1].author == $parent.currentUser._id ? 'Vous' : conv.name }}: {{conv.messages[conv.messages.length - 1].content}}</p>
                             </div>
                         </div>
                     </div>
-                    <div class="column">
-                        <h2 class="title white-title is-4">
-                            Messages
-                        </h2>
+                    <div class="modimo-messages">
                         <div>
                             <div id="thread-content" class="thread-container">
                                 <div v-for="message in currentConv.messages" :key="message.timestamp" :class="message.author != $parent.currentUser._id ? 'message-block' : 'message-block-yours'" >
                                     <p :class="message.author != $parent.currentUser._id ? 'message' : 'message-yours'" >{{ message.content }}</p>
                                 </div>
-                                <span v-if="currentConv && !currentConv.messages.length">{{ $t("messagerie.thread.no_messages") }}</span>
+                                <span v-if="currentConv && (!currentConv.messages || !currentConv.messages.length)" style="color: gray">Aucun message</span>
                             </div>
-                            <div v-if="currentConv" class="columns">
-                                <div class="column is-12-mobile is-8-tablet">
+                            <div v-if="currentConv" class="columns is-mobile">
+                                <div class="column is-8">
                                     <input class="input-message" @keyup.enter="sendMessage()" v-model="message" placeholder="Message">
                                 </div>
-                                <div class="column is-12-mobile is-4-tablet">
+                                <div class="column is-4">
                                     <div class="button" style="width: 100%" @click="sendMessage()">Envoyer</div>
                                 </div>
                             </div>
