76c8b7a5c78d9524c481ce852addc6e54206c288
jurgen <jurgen.palsma@gmail.com>
2018-11-24 13:02:51 +0100

    fix localhost

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export default() => {
     return axios.create({
-        //baseURL: 'https://modimo-beta-api.herokuapp.com/'
-        baseURL: 'http://localhost:8000'
+        baseURL: 'https://modimo-beta-api.herokuapp.com/'
+        //baseURL: 'http://localhost:8000'
     })
 }
