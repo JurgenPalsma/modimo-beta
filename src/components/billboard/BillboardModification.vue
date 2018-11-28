efa42af3c3dc5f1291710125cb0523bd7afaf93b
Raphael <raphael.moisset.grimm@gmail.com>
2018-11-28 18:04:25 +0100

    removing console error + addind author name + correcting a tile to appear as it shouldn't

--- src/components/billboard/BillboardModification.vue
+++ src/components/billboard/BillboardModification.vue
@@ -1,46 +1,58 @@
 <template>
-    <section>
-        <div class="modal is-active">
-            <div class="modal-background" @click="$emit('close_modal')"></div>
-                <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">Modifier L'affiche</p>
-                    <button class="delete" aria-label="close" @click="$emit('close_modal')"></button>
-                </header>
-                <section class="modal-card-body">
-                    <div class="field">
-                        <label class="label">Titre</label>
-                        <div class="control">
-                            <input class="input" type="text" v-model="info.title" placeholder="Titre de votre affiche...">
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <label class="label">Information</label>
-                        <div class="control">
-                            <textarea class="textarea" v-model="info.content" placeholder="Information de votre affiche..."></textarea>
-                        </div>
-                    </div>                
-                </section>
-                <footer class="modal-card-foot">
-                    <button class="button is-success" @click="updateInformation(info._id, info.title, info.content)">Modifier</button>
-                    <button class="button" @click="$emit('close_modal')">Annuler</button>
-                </footer>
+  <section>
+    <div class="modal is-active">
+      <div class="modal-background" @click="$emit('close_modal')"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Modifier L'affiche</p>
+          <button class="delete" aria-label="close" @click="$emit('close_modal')"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
+            <label class="label">Titre</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                v-model="info.title"
+                placeholder="Titre de votre affiche..."
+              >
             </div>
-        </div>
-    </section>
+          </div>
+
+          <div class="field">
+            <label class="label">Information</label>
+            <div class="control">
+              <textarea
+                class="textarea"
+                v-model="info.content"
+                placeholder="Information de votre affiche..."
+              ></textarea>
+            </div>
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button
+            class="button is-success"
+            @click="updateInformation(info._id, info.title, info.content)"
+          >Modifier</button>
+          <button class="button" @click="$emit('close_modal')">Annuler</button>
+        </footer>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import BillboardService from "@/services/BillboardService";
+import UserService from "@/services/UserService";
 
 export default {
   name: "BillboardModification",
   props: ["info"],
+
   data() {
     return {
-      title: "",
-      content: "",
       isActive: true
     };
   },
@@ -70,7 +82,6 @@ export default {
         content
       ).then(response => {
         if (response.data.success) {
-          this.load();
           this.$parent.notification = {
             type: "success",
             message: "Update Information Success"
