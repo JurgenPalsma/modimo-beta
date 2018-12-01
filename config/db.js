03ca78bcad4e6900f04ef1db6e468b9f9c40d4fb
jurgen <jurgen.palsma@gmail.com>
2018-12-01 17:25:57 +0100

    secret

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
 
     // mongo db url
-    url : "mongodb://modimo-dev:modimoteam123!@ds135394.mlab.com:35394/modimo-messagerie"
+    url : process.env.DB_URL//"mongodb://modimo-dev:modimoteam123!@ds135394.mlab.com:35394/modimo-messagerie"
 };
